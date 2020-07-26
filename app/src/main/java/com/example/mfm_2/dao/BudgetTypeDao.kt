fdcfaf985c2ea54bd33e52c11e7cc81cff5ae668
Umar <kedaitayar09@gmail.com>
2020-07-26 20:51:43 +0800

    restructure some of the package, change model to entity, clean up the SelectedDate stuff by removing the use of SelectedDateSingleton.kt

--- app/src/main/java/com/example/mfm_2/dao/BudgetTypeDao.kt
+++ app/src/main/java/com/example/mfm_2/dao/BudgetTypeDao.kt
@@ -2,7 +2,7 @@ package com.example.mfm_2.dao
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
-import com.example.mfm_2.model.BudgetType
+import com.example.mfm_2.entity.BudgetType
 
 @Dao
 interface BudgetTypeDao {
